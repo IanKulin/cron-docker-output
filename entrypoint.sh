0a50e52651c46aefe270b66076eee5512484b0e2
Ian Bailey <IanKulin@kulin.com.au>
2024-07-02 15:16:45 +0800

    + comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# create the crontab entry
 echo "* * * * * /script.sh > /proc/1/fd/1 2>&1" | crontab -
+
+# start cron in foreground to keep container running indefinitely
 echo Starting
 cron -f
